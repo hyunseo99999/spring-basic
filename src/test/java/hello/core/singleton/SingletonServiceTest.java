120a1170bedb1d0609db320eb6302493339bcbfb
hsshin <hyunseo99999@naver.com>
2022-05-23 22:03:14 +0900

    스프링컨테니어 test

---
+++ src/test/java/hello/core/singleton/SingletonServiceTest.java
@@ -0,0 +1,24 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SingletonServiceTest {
+
+    @Test
+    @DisplayName("스프링 컨테이너와 의존성 주입")
+    void springContainer() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
+    }
+
+}
